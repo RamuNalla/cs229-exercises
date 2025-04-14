fddca7091ae168802b714ae03e7dde44ce6b01dc
Mythri Kongathi <nallaramu4321@gmail.com>
2025-04-14 21:45:57 +0530

    Added Gaussian discriminant analysis in week-2

--- week-2/logistic_regression_mlflow_project/logistic_train_mlflow.py
+++ week-2/logistic_regression_mlflow_project/logistic_train_mlflow.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score
 
 mlflow.set_tracking_uri("./mlruns")
 
-experiment_name = "logistic_regression_experiment_3"
+experiment_name = "logistic_regression_experiment_4"
 experiment_id = mlflow.create_experiment(experiment_name) if mlflow.get_experiment_by_name(experiment_name) is None else mlflow.get_experiment_by_name(experiment_name).experiment_id    #create a new id if the name doesn't exist. If the name exists, get that id and assign it to experiment_id
 
 mlflow.set_experiment(experiment_name)
@@ -16,10 +16,10 @@ X, y = make_blobs(n_samples=1000, centers=2, random_state=42, cluster_std=3.5)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Start a new MLflow run
-with mlflow.start_run(run_name = "run_with_C = 0.5"):
+with mlflow.start_run(run_name = "run_with_C = 1.5"):
     
     # Hyperparameter
-    C = 0.5  # Regularization strength
+    C = 1.5  # Regularization strength
     model = LogisticRegression(C=C, solver='lbfgs')
 
     # Training
